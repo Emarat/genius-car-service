038d74168e84d2437dbe6a512b1255eaff248267
Emarat <emarathossain00@gmail.com>
2022-04-16 01:47:18 +0600

    banner

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -11,8 +11,8 @@ const Services = () => {
             .then(data => setServices(data))
     }, [])
     return (
-        <div>
-            <h1 className='services-title'> Our Services</h1>
+        <div className='container'>
+            <h1 className='services-title mt-5'> Our Services</h1>
             <div className="services-container">
                 {
                     services.map(service => <Service
